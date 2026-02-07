a128f03a4540638eaf727c76a0fe85b0dc27e205
samanda293 <144985891+samanda293@users.noreply.github.com>
2026-02-07 15:30:53 +0530

    Refactor image array and update opacity timing

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-const images = [
-  "p1.jpg",
-  "p2.jpg",
-  "p3.jpg",
-  "p4.jpg"
-];
-
+const images = ["p1.jpg", "p2.jpg", "p3.jpg", "p4.jpg"];
 let index = 0;
+
 const pizzaImage = document.getElementById("pizzaImage");
 
 setInterval(() => {
-  pizzaImage.style.opacity = 0;
+    pizzaImage.style.opacity = 0;
 
-  setTimeout(() => {
-    index = (index + 1) % images.length;
-    pizzaImage.src = images[index];
-    pizzaImage.style.opacity = 1;
-  }, 400);
+    setTimeout(() => {
+        index = (index + 1) % images.length;
+        pizzaImage.src = images[index];
+        pizzaImage.style.opacity = 1;
+    }, 500);
 
 }, 3000);
